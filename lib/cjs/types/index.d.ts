91d3a55efc70e6b00856ba4b686192a25d735144
TheGreatAxios <sawyer@dirtroad.dev>
2023-07-24 08:53:28 -0700

    Setup Package Build Scripts

---
+++ lib/cjs/types/index.d.ts
@@ -0,0 +1,4 @@
+declare const hello: () => void;
+export default hello;
+export declare const foo = "bar";
+//# sourceMappingURL=index.d.ts.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const hello = () => {
+    console.log("Hello World");
+}
+
+export default hello;
+
+export const foo = "bar";
